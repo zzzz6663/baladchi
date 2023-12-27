1be87ae21bee984f501c525ea93971afe132e4e4
naser <na3r.jafari@gmail.com>
2023-12-27 16:06:48 +0330

    a4

--- public/build/assets/app.c968aea3.js
+++ public/build/assets/app.c968aea3.js
@@ -1,7 +1,7 @@
 var cv=(h,m)=>()=>(m||h((m={exports:{}}).exports,m),m.exports);var qm=cv((sf,af)=>{var pi=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{};function Dl(h){return h&&h.__esModule&&Object.prototype.hasOwnProperty.call(h,"default")?h.default:h}var lu={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
- * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
+ * Copyright OpenJS Foundatison and other contributors <https://openjsf.org/>
  * Released under MIT license <https://lodash.com/license>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
  * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
