fc5c43fce893d8525bce38934b87f4cc0350f4a0
naser <na3r.jafari@gmail.com>
2024-01-14 11:42:38 +0330

    day241

--- resources/views/main/site.blade.php
+++ resources/views/main/site.blade.php
@@ -6,8 +6,8 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    {{--  <title>@yield('title')</title>  --}}
-    <title>711712</title>
+    <title>@yield('title')</title>
+    {{--  <title>711712</title>  --}}
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{--  <link rel="stylesheet" href="/home/css/style.css">  --}}
