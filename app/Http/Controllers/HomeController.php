5d58c0e0f86ee7b5f02c19214132d2b24436abd9
naser <na3r.jafari@gmail.com>
2024-08-08 15:44:36 +0330

    mor1910

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -533,7 +533,7 @@ class HomeController extends Controller
         $user = auth()->user();
         $last_ads= Advertise::where('status', 'confirmed')->whereActive('1')->latest()->take(5)->get();
     $baladchies=User::whereRole("user")->where('baladchi', '!=', null)->latest()->take(4)->get();;
-
+//  
          $counsels = Counsel::query();
          $counsels->whereNull("removed");
          $counsels->whereStatus('show');
